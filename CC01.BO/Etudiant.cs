6dccb6dd4dfe1a188db6de323362f8f12f42e8aa
cedrdjio <69305726+cedrdjio@users.noreply.github.com>
2020-11-27 01:17:21 +0100

    classe fine

--- CC01.BO/Etudiant.cs
+++ CC01.BO/Etudiant.cs
@@ -13,7 +13,7 @@ namespace CC01.BO
         public string Prenom;
         public DateTime Date_naissance;
         public string Lieu_naissance;
-        public string Contact;
+        public string  Contact;
         public string Email;
         public byte[] Photo { get; set; }
 
@@ -22,5 +22,43 @@ namespace CC01.BO
 
         }
 
+        public Etudiant(string nom, string prenom, DateTime date_naissance, string lieu_naissance, string contact, string email, string text, byte[] photo)
+        {
+            Nom = nom;
+            Prenom = prenom;
+            Date_naissance = date_naissance;
+            Lieu_naissance = lieu_naissance;
+            Contact = contact;
+            Email = email;
+            Photo = photo; 
+        }
+
+        public Etudiant(string nom, string prenom, DateTime date_naissance, string lieu_naissance, string contact, string email, string matricule)
+        {
+            this.Matricule = matricule;
+            this.Nom = nom;
+            this.Prenom = prenom;
+            this.Date_naissance = date_naissance;
+            this.Lieu_naissance = lieu_naissance;
+            this.Contact = contact;
+            this.Email = email;
+        }
+
+        public object Non { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Etudiant etudiant &&
+                   Matricule == etudiant.Matricule &&
+                   Date_naissance == etudiant.Date_naissance;
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = 168587438;
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Matricule);
+            hashCode = hashCode * -1521134295 + Date_naissance.GetHashCode();
+            return hashCode;
+        }
     }
 }
